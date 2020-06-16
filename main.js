42f056da1bdea0720f36868993bca339a7964bac
Sebastian Misztal <sepczak@gmail.com>
2020-06-16 22:02:58 +0200

    week 4 - BEM, block, JS querySelector

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const name = 'jakaś nazwa';
-const age = 34;
+let name = 'Krzyś';
+let age = 34;
 
 console.log('jo ' + name + age);
 console.log(`jo ${name} ${age}`);
+
+name = 'Tomcio';
+
+age = 1000;
+
+const element = document.querySelector('.section__item--js');
+
+element.innerHTML = `Ten element wygenerowny został za pomocą JS wraz ze zmiennymi ${name} ${age}`;
