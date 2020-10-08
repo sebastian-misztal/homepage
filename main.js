e0afe61ad5279705f6438c3642ad81ae391dd46b
Sebastian Misztal <sepczak@gmail.com>
2020-10-08 14:35:23 +0200

    Praca domowa tydzień 6

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ age = 1000;
 const element = document.querySelector('.section__item--js');
 
 element.innerHTML = `Ten element wygenerowny został za pomocą JS wraz ze zmiennymi ${name} ${age}`;
+
+
+function welcome(name, age) {
+    console.log(`Witaj. Mam na imię ${name} i mam ${age} lat.`)
+}
+
+welcome(name, age);
+
+const hamburger = document.querySelector('.hamburger--js');
+
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.menu--js');
+    nav.classList.toggle('menu--open')
+})
